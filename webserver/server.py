ad92d0185c0a7a3040dbedcc72bf7067ff73a29a
lampard <ds3717@columbia.edu>
2019-04-16 01:14:20 -0400

    test

--- webserver/server.py
+++ webserver/server.py
@@ -19,6 +19,7 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response, flash, session
+import random
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -127,17 +128,21 @@ def index():
     WHERE O.NO = T.order_NO
     AND O.NO = F.order_NO
     AND O.NO = D.order_NO
-    AND T.customer_id = \'%s\'
-    """ % session['user']
+    AND T.customer_id = %s
+    """
     print session['user']
-    cursor = g.conn.execute(cmd)
+    cursor = g.conn.execute(cmd, (session['user']))
     names = []
     for result in cursor:
       print result
       names.append(list(result))  # can also be accessed using result[0]
     print names
     cursor.close()
-    context = dict(data=names)
+    driver_id = []
+    for i in names:
+      if i[4] not in driver_id:
+        driver_id.append(i[4])
+    context = dict(data=names, did=driver_id)
     return render_template('index.html', **context)
   #
   # example of a database query
@@ -195,6 +200,143 @@ def index():
 def another():
   return render_template("anotherfile.html")
 
+@app.route('/gotoregister')
+def gotoregister():
+  return render_template("register.html")
+
+@app.route('/gotoindex')
+def gotoindex():
+  return redirect('/')
+
+
+@app.route('/gotoorder')
+def gotoorder():
+  cmd = """
+With Score AS(
+SELECT F.restaurant_name, AVG(O.food_score) rating
+FROM Order_from F, Orders O
+WHERE F.order_NO = O.NO
+GROUP BY F.restaurant_name) 
+
+SELECT R.name, N.state, R.boss, S.rating
+FROM Restaurant R, Region N, Locate_in L, Score S
+WHERE R.name = L.restaurant_name
+AND L.region_id = N.id
+AND S.restaurant_name = R.name
+Order BY S.rating DESC
+  """
+  cursor = g.conn.execute(cmd)
+  names = []
+  for result in cursor:
+    print result
+    names.append(list(result))  # can also be accessed using result[0]
+  for i in range(len(names)):
+    names[i][0] = names[i][0].replace(" ", "_")
+  print names
+  cursor.close()
+  context = dict(data=names)
+  return render_template("order.html", **context)
+
+
+@app.route('/filterlocation', methods=['POST'])
+def filterlocation():
+  st = request.form['state']
+  cmd = """
+With Score AS(
+SELECT F.restaurant_name, AVG(O.food_score) rating
+FROM Order_from F, Orders O
+WHERE F.order_NO = O.NO
+GROUP BY F.restaurant_name) 
+
+SELECT R.name, N.state, R.boss, S.rating
+FROM Restaurant R, Region N, Locate_in L, Score S
+WHERE R.name = L.restaurant_name
+AND L.region_id = N.id
+AND S.restaurant_name = R.name
+AND N.state = %s
+Order BY S.rating DESC
+  """
+  cursor = g.conn.execute(cmd, (st,))
+  names = []
+  for result in cursor:
+    # print result
+    names.append(list(result))  # can also be accessed using result[0]
+  for i in range(len(names)):
+    names[i][0] = names[i][0].replace(" ", "_")
+  print names
+  cursor.close()
+  context = dict(data=names)
+  return render_template("order.html", **context)
+
+
+@app.route('/searchrestaurant', methods=['POST'])
+def searchrestaurant():
+  st = request.form['key'].strip()
+  print st
+  cmd = """
+  With Score AS(
+  SELECT F.restaurant_name, AVG(O.food_score) rating
+  FROM Order_from F, Orders O
+  WHERE F.order_NO = O.NO
+  GROUP BY F.restaurant_name) 
+  
+  SELECT R.name, N.state, R.boss, S.rating
+  FROM Restaurant R, Region N, Locate_in L, Score S
+  WHERE R.name = L.restaurant_name
+  AND L.region_id = N.id
+  AND S.restaurant_name = R.name
+  AND R.name LIKE %s
+  Order BY S.rating DESC"""
+  print cmd
+  # sql = "SELECT * FROM things WHERE description LIKE %(like)s ESCAPE '='"
+  # cursor.execute(sql, dict(like='%' + term + '%'))
+  # cursor = g.conn.execute(cmd, ('%'+st+'% ',))
+  cursor = g.conn.execute(cmd, ('%'+st+'%',))
+  names = []
+  for result in cursor:
+    # print result
+    names.append(list(result))  # can also be accessed using result[0]
+  for i in range(len(names)):
+    names[i][0] = names[i][0].replace(" ", "_")
+  print names
+  cursor.close()
+  context = dict(data=names)
+  return render_template("order.html", **context)
+
+
+@app.route('/seedriver', methods=['POST'])
+def seedriver():
+  st = request.form['driver_id']
+  print st
+  cmd_driver_info = """
+  SELECT D.id, D.name, D.phone, C.name, C.location
+  FROM Driver D, Belong_to B, Delivery_company C
+  WHERE D.id = %s
+  AND D.id = B.driver_id
+  AND B.delivery_company_name = C.name
+  """
+  cursor = g.conn.execute(cmd_driver_info, (st,))
+  info = []
+  for result in cursor:
+    # print result
+    info.append(list(result))  # can also be accessed using result[0]
+  print info
+  cursor.close()
+  cmd_deliver = """
+  SELECT O.no, O.time Order_time, D.time Deliver_time, driver_score
+  FROM orders O, deliver D
+  WHERE D.driver_id = %s
+  AND O.no = D.order_no
+  """
+  cursor = g.conn.execute(cmd_deliver, (st,))
+  delivers = []
+  for result in cursor:
+    # print result
+    delivers.append(list(result))  # can also be accessed using result[0]
+  print delivers
+  cursor.close()
+  context = dict(data=info, deliver=delivers)
+  return render_template("deliver.html", **context)
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
@@ -214,14 +356,111 @@ def rate():
   print no, f_score, d_score
   cmd = """
   UPDATE Orders
-SET food_score = %f,
- driver_score = %f
+SET food_score = %s,
+ driver_score = %s
 WHERE
- Orders.NO = \'%s\'
- """ %(f_score, d_score, no)
+ Orders.NO = %s
+ """
   print cmd
-  g.conn.execute(text(cmd))
+  g.conn.execute(cmd, (f_score, d_score, no,))
   return redirect('/')
+
+
+@app.route('/pay', methods=['POST'])
+def pay():
+  r_name = request.form['restaurant name'].replace("_", " ")
+  i_name = request.form['item name'].replace("_", " ")
+  print r_name
+  print i_name
+  print session['user']
+  cursor = g.conn.execute("SELECT DISTINCT NO FROM orders")
+  orderid = []
+  for result in cursor:
+    print result
+    orderid.append(int(result['no']))  # can also be accessed using result[0]
+  print max(orderid)
+  new_id = max(orderid) + 1
+  cursor.close()
+  hour = random.randint(0, 23)
+  minute = random.randint(0, 40)
+  cmd_orders = """
+  INSERT INTO orders(no, time) VALUES
+  (%s, %s)
+  """
+  cursor = g.conn.execute(cmd_orders, (str(new_id), str(hour) + ":" + str(minute)))
+  cursor.close()
+  cmd_to_order = """
+  INSERT INTO to_order(customer_id, order_no) VALUES
+  (%s, %s) 
+
+  """
+  cursor = g.conn.execute(cmd_to_order,(session['user'], str(new_id)))
+  cursor.close()
+  cmd_order_from = """
+  INSERT INTO order_from(order_no, item_name, restaurant_name) VALUES
+  (%s, %s, %s)
+  """
+  cursor = g.conn.execute(cmd_order_from, (str(new_id), i_name, r_name))
+  cursor.close()
+  cursor = g.conn.execute("SELECT DISTINCT driver_id FROM belong_to")
+  driver_ids = []
+  for result in cursor:
+    print result
+    driver_ids.append(result['driver_id'])  # can also be accessed using result[0]
+  driver_idx = random.randint(0, len(driver_ids) - 1)
+  cmd_deliver = """
+  INSERT INTO deliver(order_no, time, driver_id) VALUES
+  (%s, %s, %s)
+  """
+  cursor = g.conn.execute(cmd_deliver, (str(new_id), str(hour) + ":" + str(minute + 15), driver_ids[driver_idx]))
+  cursor.close()
+
+#   no = request.form['no']
+#   f_score = float(request.form['food_score'])
+#   d_score = float(request.form['driver_score'])
+#   print no, f_score, d_score
+#   cmd = """
+#   UPDATE Orders
+# SET food_score = %f,
+#  driver_score = %f
+# WHERE
+#  Orders.NO = \'%s\'
+#  """ %(f_score, d_score, no)
+#   print cmd
+#   g.conn.execute(text(cmd))
+  return redirect('/')
+
+
+@app.route('/torestaurant', methods=['POST'])
+def torestaurant():
+  name = request.form['name'].replace("_", " ")
+  print name
+  cmd = """
+With Score AS(
+SELECT F.restaurant_name, F.item_name, AVG(O.food_score) rating
+FROM Orders O, Order_from F
+WHERE F.restaurant_name = %s
+AND F.order_NO = O.NO
+GROUP BY F.restaurant_name, F.item_name
+)
+SELECT I.restaurant_name, I.name, I.price, S.rating
+FROM Item_made_by I, Score S
+WHERE I.restaurant_name = %s
+AND I.name = S.item_name
+ORDER BY S.rating DESC
+ """
+  cursor = g.conn.execute(cmd, (name, name))
+  names = []
+  for result in cursor:
+    print result
+    names.append(list(result))  # can also be accessed using result[0]
+  for i in range(len(names)):
+    names[i][0] = names[i][0].replace(" ", "_")
+    names[i][1] = names[i][1].replace(" ", "_")
+  print names
+  cursor.close()
+  context = dict(data=names)
+  return render_template('menu.html', **context)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -233,10 +472,10 @@ def login():
 # try:
 #   name = "kb3467"
 #   passw = "Kobe Bryant"
-  cmd = "SELECT Count(1) FROM customer WHERE id = \'%s\' and password = \'%s\'" % (name, passw)
+  cmd = "SELECT Count(1) FROM customer WHERE id = %s and password = %s"
   # cursor = g.conn.execute("SELECT NO FROM Orders")
   # print cmd
-  cursor = g.conn.execute(cmd)
+  cursor = g.conn.execute(cmd, (name, passw))
   # print list(cursor)
   r = False
   for result in cursor:
@@ -252,6 +491,27 @@ def login():
   return index()
   # except:
   #   return "except"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if session['logged_in']:
+    return render_template('/')
+  else:
+    _id = request.form['id']
+    name = request.form['name']
+    passw = request.form['password']
+    cmd = """
+    INSERT INTO customer VALUES
+    (%s, %s, %s)
+    """
+    try:
+      cursor = g.conn.execute(cmd, (_id, name, passw))
+      flash("Register Success!")
+      return redirect('/')
+    except:
+      flash("Id already used!")
+      return render_template('register.html')
 
 
 @app.route("/logout")
